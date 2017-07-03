0f090dc38b0ec03d87fbd4b5a34fc1ab8bb499e3
Alexey Melnichuk <alexeymelnichuck@gmail.com>
2017-07-03 11:03:31 +0300

    Add. Dump example printout sensor unit name

--- examples/dump.lua
+++ examples/dump.lua
@@ -4,6 +4,19 @@ package.path = "..\\src\\?.lua;" .. package.path
 
 local ohm = require "OpenHardwareMonitor"
 
+local sensor_type_units = {
+  Clock       = {"MHz",  "Megahertz"              };
+  Control     = {"%",    "Percentage"             };
+  Data        = {"GB",   "Gigabyte"               };
+  Fan         = {"RPM",  "Revolutions per minute" };
+  Flow        = {"L/h",  "Liters per hour"        };
+  Level       = {"%",    "Percentage"             };
+  Load        = {"%",    "Percentage"             };
+  Power       = {"W",    "Wat"                    };
+  Temperature = {"C",    "Celsius"                };
+  Voltage     = {"V",    "Volt"                   };
+}
+
 local function printf(...)
   print(string.format(...))
 end
@@ -18,8 +31,11 @@ local function print_tree(t, indent)
     if node.sensors then
       for sensor_type, sensors in pairs(node.sensors) do
         printf("%s  %s", indent, sensor_type)
+        local unit = sensor_type_units[sensor_type]
+        unit = unit and unit[1] or ""
+        if unit ~= '%' then unit = ' ' .. unit end
         for _, sensor in ipairs(sensors) do
-          printf("%s    %s (%s) = %0.2f", indent, sensor.name, sensor.identifier, sensor.value)
+          printf("%s    %s (%s) = %0.2f%s", indent, sensor.name, sensor.identifier, sensor.value, unit)
         end
       end
     end
